caceaff81cf1795275982349f698b5b5f0e0df85
Sean Keeley <sean.keeley@teamaol.com>
2016-11-03 14:20:13 +0000

    Synced for Gavin JS

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
-//Coding Camp 2016
-//Sean Keeley
+$(function() {
+
+  var counter = 0;
+  var button = $('.button');
+  var container = $('.counter');
+  var gdlogo = $('.gdlogo');
+
+  button.click(function(){
+    counter++;
+    container.html(counter);
+  });
+
+  $('.div1').on( "mouseover", function() {
+    $('.div1').css( "color", "red" );
+  });
+
+});
